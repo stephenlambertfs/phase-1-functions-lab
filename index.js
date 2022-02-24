60df07c9e9cf79e10b79de37560983b14f1c8457
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-02-24 16:53:45 -0700

    commit tree 3c4b2d293773b6781062a5497df644da3bf48149

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(passenger){
+    let blocks = passenger - 42;
+    if(passenger < 42){
+        let blocks = 42-passenger;
+        return blocks;
+    }
+    return blocks;
+
+}
+
+function distanceFromHqInFeet(feet){
+    let feets = distanceFromHqInBlocks(feet) * 264;
+    return feets;
+}
+
+function distanceTravelledInFeet(start, end){
+    let dists = (end-start) * 264;
+    if(dists < 264){
+        let dist = (start - end) * 264;
+        return dist;
+
+    }
+    return dists;
+}
+
+function calculatesFarePrice(start, destination){
+    let dist = (start - destination) * 264;
+    if (dist < 400) {
+        return 0;
+    }if (dist > 400 && dist < 2000) {
+        return ((dist - 400) * .02);
+    }if (dist > 2000) {
+        return 25;
+    }else {
+        return ('cannot travel that far')
+    }
+    if(true){
+
+    }
+}
+
